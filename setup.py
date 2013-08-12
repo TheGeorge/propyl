68953e2c16ac94a3038c7f20fbb39d33b1f5c271
Andreas Löscher <andreas.loescher@it.uu.se>
2013-08-12 16:38:26 +0200

    install script

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name="Pyper",
+			version=0.1,
+			description="PYthon ProPER property based stateful testing",
+			author="Andreas Löscher",
+			author_email="andreas.loscher@it.uu.se",
+			packages=["pyper"]
+			)
